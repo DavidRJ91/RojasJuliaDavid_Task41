c4f208810f363255e0d9bbd691353b79dc65315e
David Rojas Julia <32340635+DavidRJ91@users.noreply.github.com>
2017-10-25 20:43:20 +0200

    Update TestVehicle.java

--- src/TestVehicle.java
+++ src/TestVehicle.java
@@ -1,8 +1,30 @@
 import transport.Vehicle;
-
+/**
+ * Aqui realizamos las pruebas y realizamos la impresiones en pantalla.
+ */
 public class TestVehicle {
     static public void main(String args[]) {
         Vehicle myVehicle = new Vehicle(10000);
-        System.out.println(myVehicle.getMaxLoad());
+        System.out.println("La capacidad del vehiculo es "+ myVehicle.getMaxLoad() + " KG");
+
+        System.out.println(myVehicle.addBox(500));
+
+        System.out.println(myVehicle.addBox(250));
+
+        System.out.println(myVehicle.addBox(5000));
+
+        System.out.println(myVehicle.addBox(6000));
+
+        System.out.println(myVehicle.addBox(4000));
+
+        System.out.println(myVehicle.addBox(300));
+
+        System.out.println("La carga total del camion es "+ myVehicle.getLoad() + "KG");
+
     }
 }
+
+/**
+ * David Rojas Julia
+ * 2º ASIX
+ */
